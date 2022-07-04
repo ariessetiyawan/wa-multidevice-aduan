22a2c8fd12e07944794042fbd0e61cbb40930231
aries setiyawan <99067179+ariessetiyawan@users.noreply.github.com>
2022-07-04 10:32:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ const hss =await bacaAllAReply()
 
 const listenerCallback = () => {
     init()
-    console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
+    //console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
 }
 
 if (host) {
-    app.listen(port, host, listenerCallback)
+    app.listen( host, listenerCallback)
 } else {
-    app.listen(port, listenerCallback)
+    app.listen( listenerCallback)
 }
 
 nodeCleanup(cleanup)
