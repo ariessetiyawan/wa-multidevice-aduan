598d6a677127c49e93ce8a48fc4a796aa901d226
aries setiyawan <99067179+ariessetiyawan@users.noreply.github.com>
2022-07-25 01:56:47 +0700

    Update getUserSetting.js

--- controllers/getUserSetting.js
+++ controllers/getUserSetting.js
@@ -5,8 +5,9 @@ const getUsersetting = async (req, res) => {
 	try{
 		const idgas = req.body.idgas
 		const sessionid = req.body.sessionid
+        console.log('group',group)
 		settingall=await bacaGroupUser(group)
-		//console.log('settingall',settingall.data.rows)
+		console.log('settingall',settingall.data.rows)
 		if (settingall.data.rows.length>0){
 			let idr =settingall.data.rows.filter(it => it.SESSION === sessionid);
 			if (idr.length>0){
